5174da35047fa2c1eddb4572e7311a0d66c16a19
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-02-28 22:51:13 -0500

    First Commit

--- homework_week3_day2.py
+++ homework_week3_day2.py
@@ -18,8 +18,28 @@ class Employee:
         print(f"{self.first_name.title()} {self.last_name.title()} got a raise.")
         print(f"Their new salary is {self.salary}.")
 
-employee1 = Employee("roy", "bland", "head of iron curtain networks", 30000, "none")
+class Sales(Employee):
+    def __init__(self, first_name, last_name, title, salary, email, phone_number):
+        super().__init__(first_name, last_name, title, salary, email)
+        self.phone_number = phone_number
+
+    def followup_email(self, customer_name):
+        print(f"Dear {customer_name.title()}, Thank you for reaching out. My associates will be in touch shortly.")
+        print(f"In the meantime, you can contact me by phone at {self.phone_number} or by email at {self.email}.")
+
+class Development(Employee):
+    def __init__(self, first_name, last_name, title, salary, email):
+        super().__init__(first_name, last_name, title, salary, email)
+
+    def code(self):
+        print(f"{self.first_name.title()} {self.last_name.title()} is writing code.")
+
+
+employee1 = Sales("wilfred", "sinecure", "head of customer relations", 50000, "wsinecure@org.org", "(123) 456 7890")
+employee1.followup_email("Mike O'Neill")
+employee1.followup_email("Hannah Stern")
 employee1.employee_raise()
 
-
-
+employee2 = Development("monica", "tulayev", "sr. engineer", 100000, "mtulayev@org.org")
+employee2.code()
+employee2.employee_raise()
